9338e9f3882be9d99266687d66cc7d4037362ee1
Ivan Bityutskiy <73762373+ivbit@users.noreply.github.com>
2026-01-05 22:29:57 +0300

    software

---
+++ hello.erl
@@ -0,0 +1,23 @@
+%% -*- coding: utf-8 -*-
+-module(hello).
+-export([start/0]).
+
+
+
+% To compile from Erlang shell:
+% c(hello).
+% hello:start().
+% halt().
+
+% To compile from OS shell:
+% erlc hello.erl
+% erl -noshell -s hello start -s init stop
+
+
+
+start() ->
+    io:format("Hello, World!~n"),
+    io:format("~s~n", [<<"Привет, Мир!"/utf8>>]).
+
+
+
